fb7eebb6d2bc0fad03e3d11b28699579c984d92e
Bruno-Gsilva <brunogomesdsilva8@gmail.com>
2023-08-18 18:01:12 -0300

    feat:dashboard

--- src/pages/Home/Dashboard/index.tsx
+++ src/pages/Home/Dashboard/index.tsx
@@ -1,11 +1,83 @@
-import React from "react";
+import  { useState, useEffect } from 'react';
+import { Chart } from 'primereact/chart';
 
-const PageDashboard: React.FC  = () => {
+export default function PageDashboard() {
+    const [chartData, setChartData] = useState({});
+    const [chartOptions, setChartOptions] = useState({});
+
+    useEffect(() => {
+        const data = {
+            labels: ['destreza', 'attack', 'força', 'Inteligencia'],
+            datasets: [
+                {
+                    label: 'Monster',
+                    data: [540, 925, 702, 620],
+                    backgroundColor: [
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)'
+                      ],
+                      borderColor: [
+                        'rgb(255, 159, 64)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)'
+                      ],
+                      borderWidth: 1
+                },
+                {
+                    label: 'Mage',
+                    data: [240, 125, 202, 320],
+                    backgroundColor: [
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)'
+                      ],
+                      borderColor: [
+                        'rgb(255, 159, 64)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)'
+                      ],
+                      borderWidth: 1
+                },
+                {
+                    label: 'Monge',
+                    data: [240, 125, 202, 320],
+                    backgroundColor: [
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)'
+                      ],
+                      borderColor: [
+                        'rgb(255, 159, 64)',
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                        'rgb(153, 102, 255)'
+                      ],
+                      borderWidth: 1
+                }
+            ]
+        };
+        const options = {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        };
+
+        setChartData(data);
+        setChartOptions(options);
+    }, []);
+
     return (
-        <div className="flex-1 h-full bg-red-500">
-            dashboarddddddddddddd
+        <div className="card">
+            <Chart type="bar" data={chartData} options={chartOptions} />
         </div>
     )
 }
-
-export default PageDashboard
+        
