093d1aa3800679b00efa6804c23bd3c28ff862f6
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-08-18 15:21:50 -0300

    fix:asidebar open

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
               data-drawer-toggle="logo-sidebar"
               aria-controls="logo-sidebar"
               type="button"
-              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+              className="z-50 inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
               <span className="sr-only">Open sidebar</span>
               <AlignJustify />
             </button>
--- src/pages/Home/_layout.tsx
+++ src/pages/Home/_layout.tsx
@@ -8,8 +8,8 @@ import { AsideBar } from "../../components/AsideBar";
 const LayoutPage: React.FC = () => {
     return (
         <>
-            <Navbar />
             <AsideBar />
+            <Navbar />
             <div className="p-4 sm:ml-64">
                 <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
                     <Outlet />
