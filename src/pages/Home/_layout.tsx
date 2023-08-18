3a44df1b52e37cc5805dd03a08e1a75835a85c40
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-08-18 15:32:12 -0300

    fix:asidebar open22

--- src/pages/Home/_layout.tsx
+++ src/pages/Home/_layout.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../../components/Navbar";
-import { AsideBar } from "../../components/AsideBar";
+// import { AsideBar } from "../../components/AsideBar";
 // import { Outlet } from "react-router-dom";
 // import { Header } from "../../components/UI/Header";
 
 const LayoutPage: React.FC = () => {
     return (
         <>
-            <AsideBar />
+            {/* <AsideBar /> */}
             <Navbar />
             <div className="p-4 sm:ml-64">
                 <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
